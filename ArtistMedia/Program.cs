e3fabc402fc077521ba9e32616e97f25a1d08f55
Simon Nyssen <snyssen@neuromedia.io>
2020-08-18 10:20:22 +0200

    Add example of artist parsing

--- ArtistMedia/Program.cs
+++ ArtistMedia/Program.cs
@@ -13,19 +13,40 @@ namespace ArtistMedia
 
         static void Main(string[] args)
         {
-            using (var reader = new StreamReader(@"C:\artist.txt"))
+            using (var context = new ArtistMediaContext())
             {
-                List<string> Id = new List<string>();
-                List<string> Name = new List<string>();
-                while (!reader.EndOfStream)
+                using (var reader = new StreamReader(@"/home/snyssen/Downloads/mbdump/artist"))
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split('\t');
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        Console.WriteLine(line);
+                        var values = line.Split('\t');
+                        Console.WriteLine($"Extracted {values.Length} values");
+                        Artist art;
+                        try {
+                            art = new Artist() // See order here -> https://github.com/metabrainz/musicbrainz-server/blob/master/admin/sql/CreateTables.sql#L208
+                            {
+                                Id = int.Parse(values[0]),
+                                //Gid = new Guid(values[1]),
+                                Name = values[2],
+                                Sort_name = values[3],
+                                Begin_date_year = int.Parse(values[4])
+                                // etc.
+                            };
+                        } catch (Exception e) {
+                            Console.WriteLine("Couldn't parse all values.\n" + e.Message);
+                            continue;
+                        }
+                        Console.WriteLine($"Successfully parsed artist {art.Name}");
 
-                    int id = int.Parse(values[0]);
-                    Name.Add(values[1]);
+                        //context.Artists.Add(art); // Uncomment this when every Artist variable has been parsed
+                    }
                 }
+                //context.SaveChanges(); // Uncomment this when every Artist variable has been parsed
             }
+
+            // Then repeat this operation for every other table needed
         }
     }
     }
